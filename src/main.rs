147b35db6d7671dc2911da3d19b9e2e5d5fc1c2d
Benoît Marcelin <sereinity@sereinity.fr>
2017-11-14 00:10:24 +0100

    First simple implementation on 16 integers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello, world!");
+    let mut space = vec![false; 16];
+    for i in 2..16 {
+        sieve(&mut space, i, 2, 16);
+    }
+    display_primes(&space);
+}
+
+fn sieve(space: &mut Vec<bool>, value: usize, from: usize, to: usize) {
+    let mut i = from;
+    loop {
+        let multiple = value * i;
+        if multiple >= to {
+            break;
+        }
+
+        space[multiple] = true;
+        i += 1;
+    }
+}
+
+fn display_primes(space: &Vec<bool>) {
+    print!("Primes: ");
+    for i in 0..space.len() {
+        if !space[i] {
+            print!("{} ", i);
+        }
+    }
+    println!("");
 }
