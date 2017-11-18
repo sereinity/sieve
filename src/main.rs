e90568347e44aa63caa6863bd07473f1ff1a7a81
Benoît Marcelin <sereinity@sereinity.fr>
2017-11-18 10:44:41 +0100

    Add argument -q --no-display to prevent the print of values

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,11 @@ fn main() {
              .help("Analyze space size in power of two, ie 10, 24")
              .index(1)
             )
+        .arg(Arg::with_name("no-display")
+             .long("--no-display")
+             .short("-q")
+             .help("don't display the result")
+             )
         .get_matches();
 
     let size = value_t!(matches, "size", u32).unwrap_or_else(|e|{
@@ -24,5 +29,7 @@ fn main() {
 
     let mut space = sieve::Space::new(size);
     space.compute_all();
-    space.display_primes();
+    if !matches.is_present("no-display") {
+        space.display_primes();
+    }
 }
