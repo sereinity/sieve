e541e4d3d67f2fc3db3a8f1a26637af7ae6059b1
Benoît Marcelin <sereinity@sereinity.fr>
2017-11-18 10:37:53 +0100

    Add an argument parser, parse the size with a default to 16

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
+#[macro_use]
+extern crate clap;
 extern crate sieve;
 
+use clap::{App, Arg, ErrorKind};
+
 fn main() {
-    let mut space = sieve::Space::new(24);
+    let matches = App::new("sieve")
+        .about("Optimized Erathostenese's sieve")
+        .author("Sereinity <sereinity@online.fr>")
+        .version(crate_version!())
+        .arg(Arg::with_name("size")
+             .help("Analyze space size in power of two, ie 10, 24")
+             .index(1)
+            )
+        .get_matches();
+
+    let size = value_t!(matches, "size", u32).unwrap_or_else(|e|{
+        match e.kind {
+            ErrorKind::ArgumentNotFound => 16,
+            _ => e.exit(),
+        }
+    });
+
+    let mut space = sieve::Space::new(size);
     space.compute_all();
     space.display_primes();
 }
