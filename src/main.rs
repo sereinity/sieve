9532549b02c0d64e377e271932d854ba8e1e2b13
Sereinity <sereinity@sereinity.fr>
2023-06-17 16:38:58 +0200

    chore(clap): upgrade to v4

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,42 @@
 #[macro_use]
-extern crate clap;
-#[macro_use]
 extern crate slog;
 
-use clap::{App, Arg, ErrorKind};
+use clap::{value_parser, ArgAction, Parser};
+
+#[derive(Parser)]
+#[command(version)]
+#[command(author)]
+#[command(about = "Optimized Erathostenese's sieve")]
+struct Cli {
+    #[arg(help = "Analyze space size in power of two, ie 10, 24")]
+    #[arg(value_parser = value_parser!(u32))]
+    #[arg(default_value = "16")]
+    size: u32,
+
+    #[arg(short = 'q', long)]
+    #[arg(help = "Don't display the result")]
+    no_display: bool,
+
+    #[arg(long)]
+    #[arg(help = "Display dots instead of values")]
+    dots: bool,
+
+    #[arg(short, action = ArgAction::Count)]
+    #[arg(help = "Sets the level of verbosity")]
+    verbose: u8,
+}
 
 fn main() {
-    let matches = App::new("sieve")
-        .about("Optimized Erathostenese's sieve")
-        .author("Sereinity <sereinity@online.fr>")
-        .version(crate_version!())
-        .arg(
-            Arg::with_name("size")
-                .help("Analyze space size in power of two, ie 10, 24")
-                .index(1),
-        )
-        .arg(
-            Arg::with_name("no-display")
-                .long("--no-display")
-                .short('q')
-                .help("Don't display the result"),
-        )
-        .arg(
-            Arg::with_name("dots")
-                .long("--dots")
-                .help("Display dots instead of values"),
-        )
-        .arg(
-            Arg::with_name("v")
-                .short('v')
-                .help("Sets the level of verbosity"),
-        )
-        .get_matches();
+    let cli = Cli::parse();
 
     // First "v" adds debug, the second trace, trace is not available directly
     // As there isn't more log levels, we limits to 2 level of more verbosity
-    let mut digit_log_level = matches.occurrences_of("v") as usize;
-    if digit_log_level > 2 {
-        digit_log_level = 2;
-    }
-    let log = sieve::get_root_logger(digit_log_level);
+    let log = sieve::get_root_logger(cli.verbose.min(2) as usize);
 
-    let size = value_t!(matches, "size", u32).unwrap_or_else(|e| match e.kind {
-        ErrorKind::ArgumentNotFound => 16,
-        _ => e.exit(),
-    });
-
-    let mut space = sieve::Space::new(size, log.new(o!("Space" => 1)));
+    let mut space = sieve::Space::new(cli.size, log.new(o!("Space" => 1)));
     space.compute_all();
-    if !matches.is_present("no-display") {
-        space.display_primes(matches.is_present("dots"));
+
+    if !cli.no_display {
+        space.display_primes(cli.dots);
     }
 }
